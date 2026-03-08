ac30e61c7ee2c2ccd65fda9899f2dd57dd61f609
Dhanush <danudanee9@gmail.com>
2026-03-08 13:45:41 +0100

    montecarlo updates

--- Code_assignment/MonteCarlo.py
+++ Code_assignment/MonteCarlo.py
@@ -17,20 +17,19 @@ class MonteCarloAgent(BaseAgent):
         actions is a list of actions observed in the episode, of length T_ep
         rewards is a list of rewards observed in the episode, of length T_ep
         done indicates whether the final s in states is was a terminal state '''
-        # TO DO: Add own code
-        g = 0 
-        for i in reversed(range(len(actions))):
-            g = rewards[i] + self.gamma * g
+        
+        G = 0.0
+    
+        for t in reversed(range(len(rewards))):
+            G = rewards[t] + self.gamma * G
+            s = states[t]
+            a = actions[t]
             
-            s_i = states[i]
-            a_i = actions[i]
-            self.Q_sa[s_i, a_i] += self.learning_rate * (g - self.Q_sa[s_i, a_i])
-
+            if (s, a) not in [(states[i], actions[i]) for i in range(0, t)]:
+                self.Q_sa[s, a] += self.learning_rate * (G - self.Q_sa[s, a])
 
 def monte_carlo(n_timesteps, max_episode_length, learning_rate, gamma, 
                    policy='egreedy', epsilon=None, temp=None, plot=True, eval_interval=500):
-    ''' runs a single repetition of an MC rl agent
-    Return: rewards, a vector with the observed rewards at each timestep ''' 
     
     env = StochasticWindyGridworld(initialize_model=False)
     eval_env = StochasticWindyGridworld(initialize_model=False)
@@ -38,55 +37,52 @@ def monte_carlo(n_timesteps, max_episode_length, learning_rate, gamma,
     eval_timesteps = []
     eval_returns = []
 
-    # TO DO: Write your Monte Carlo RL algorithm here!
-    total_ts = 0
-
-    while total_ts < n_timesteps:
-        states = []
-        actions = []
-        rewards = []
-        s = env.reset()
-        states.append(s)
-        
-        for t in range(max_episode_length):
-            if total_ts % eval_interval == 0:
-                eval_returns.append(pi.evaluate(eval_env))
-                eval_timesteps.append(total_ts)
-            a = pi.select_action(s, policy, epsilon, temp)
-            s_next, r, done = env.step(a)
-            actions.append(a)
-            rewards.append(r)
-            states.append(s_next)
+    s = env.reset()
+    states = [s]
+    actions = []
+    rewards = []
+    
+    for t in range(n_timesteps):
+        if t % eval_interval == 0:
+            mean_return = pi.evaluate(eval_env, max_episode_length=max_episode_length)
+            eval_returns.append(mean_return)
+            eval_timesteps.append(t)
             
-            total_ts += 1
+        a = pi.select_action(s, policy=policy, epsilon=epsilon, temp=temp)
+        s_next, r, done = env.step(a)
+        
+        actions.append(a)
+        rewards.append(r)
+        states.append(s_next)
+        
+        if plot:
+           env.render(Q_sa=pi.Q_sa, plot_optimal_policy=True, step_pause=0.001)
+           
+        if done or len(actions) == max_episode_length:
+            pi.update(states, actions, rewards)
+            s = env.reset()
+            states = [s]
+            actions = []
+            rewards = []
+        else:
             s = s_next
 
-            if done or total_ts >= n_timesteps:
-                break
-
-        if plot:
-            env.render(Q_sa=pi.Q_sa,plot_optimal_policy=True,step_pause=0.1) # Plot the Q-value estimates during Monte Carlo RL execution
-
-                 
     return np.array(eval_returns), np.array(eval_timesteps) 
     
 def test():
-    n_timesteps = 1000
-    max_episode_length = 100
+    n_timesteps = 100000
+    max_episode_length = 1000
     gamma = 1.0
-    learning_rate = 0.1
+    learning_rate = 0.01
 
-    # Exploration
-    policy = 'egreedy' # 'egreedy' or 'softmax' 
+    policy = 'egreedy'
     epsilon = 0.1
     temp = 1.0
-    
-    # Plotting parameters
     plot = True
 
-    monte_carlo(n_timesteps, max_episode_length, learning_rate, gamma, 
+    eval_returns, eval_timesteps = monte_carlo(n_timesteps, max_episode_length, learning_rate, gamma, 
                    policy, epsilon, temp, plot)
+    print(eval_returns, eval_timesteps)
     
-            
 if __name__ == '__main__':
     test()
